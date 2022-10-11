dae40012fa810491a7dcddf301781a28e73edfc2
Elena Pastushenko <heleora_net@ukr.net>
2022-10-11 22:20:58 +0300

    creates gallery of images

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,40 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+import * as basicLightbox from 'basiclightbox';  
 
-console.log(galleryItems);
+const galleryItemsHTML = galleryItems.map((item) => {
+    return `<div class="gallery__item">
+  <a class="gallery__link" href="${item.original}">
+    <img
+      class="gallery__image"
+      src="${item.preview}"
+      data-source="${item.original}"
+      alt="${item.description}"
+    />
+  </a>
+</div>`
+}).join("");
+
+const galleryRef = document.querySelector(".gallery");
+const galleryImageRef = document.querySelector(".galerry__image");
+
+galleryRef.insertAdjacentHTML("afterbegin", galleryItemsHTML);
+
+galleryRef.addEventListener("click", onImageClick);
+
+function onImageClick(e) {
+    e.preventDefault();
+    
+    if (e.target.nodeName !== "IMG") {
+        console.log("Click not on image");
+        return;
+    }
+        
+    console.log("Click on image");
+        e.target.setAttribute("src", e.target.getAttribute("data-source"));
+
+        const instance = basicLightbox.create(e.target);
+        instance.show()
+        }
+
+
