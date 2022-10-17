016004d3c03764599b781c0c076da466262b78ca
Elena Pastushenko <heleora_net@ukr.net>
2022-10-17 13:07:14 +0300

    Adds library references to script.js

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,23 +1,32 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
-// import * as basicLightbox from 'basiclightbox';  
 
-const galleryItemsHTML = galleryItems.map((item) => {
-    return `<div class="gallery__item">
+
+const galleryItemsMarkup = galleryItems.map((item) => {
+  return `<div class="gallery__item">
   <a class="gallery__link" href="${item.original}">
-    <img
-      class="gallery__image"
-      src=${item.preview}
-      data-source=${item.original}
-      alt=${item.description}
-    />
+  <img
+  class="gallery__image"
+  src=${item.preview}
+  data-source=${item.original}
+  alt=${item.description}
+  />
   </a>
-</div>`
+  </div>`
 }).join("");
 
+
+const headRef = document.querySelector("head");
+const thisScript = document.querySelector(`script[src="js/01-gallery.js"]`);
 const galleryRef = document.querySelector(".gallery");
 
-galleryRef.insertAdjacentHTML("afterbegin", galleryItemsHTML);
+headRef.insertAdjacentHTML("beforeend", `<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/basiclightbox@5.0.4/dist/basicLightbox.min.css" />`);
+
+const scriptForAddBasicLightBox = document.createElement("script");
+scriptForAddBasicLightBox.src ="https://cdn.jsdelivr.net/npm/basiclightbox@5.0.4/dist/basicLightbox.min.js";
+thisScript.before(scriptForAddBasicLightBox);
+
+galleryRef.insertAdjacentHTML("afterbegin", galleryItemsMarkup);
 
 galleryRef.addEventListener("click", onImageClick);
 
